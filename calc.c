0773acee97a32d98658a9d624be52b8acdf49f03
afromogli <aram.paul.taieb@gmail.com>
2017-09-29 11:41:06 +0200

    Fixed pointer problem in main file for calc.c

--- calc.c
+++ calc.c
@@ -2,20 +2,20 @@
 
 float calc_resistance(int, char, float *);
 
-int main()     //Ett test program för att beräkna resistansen
+int main()     //Ett test program fï¿½r att berï¿½kna resistansen
 {
     float test[2], sum_denum=0, value=0;
-    float *array = &test; //Pekare mot test array!
+    float *array = test; //Pekare mot test array!
     int x = 0;
 
-    test[0] = 20.0;     //Test värden..
+    test[0] = 20.0;     //Test vï¿½rden..
     test[1] = 47.0;
 
     float Val = calc_resistance(2, 'P', array);
-    printf("Value for a parallel curcuit: %0.2f Ohm\n", Val);       //Skriv ut parallelt resultat med två värde siffror
+    printf("Value for a parallel curcuit: %0.2f Ohm\n", Val);       //Skriv ut parallelt resultat med tvï¿½ vï¿½rde siffror
 
     Val = calc_resistance(2, 'S', array);
-    printf("Value for a serial curcuit: %0.2f Ohm\n", Val);         //Skriv ut seriellt resultat med två värde siffror
+    printf("Value for a serial curcuit: %0.2f Ohm\n", Val);         //Skriv ut seriellt resultat med tvï¿½ vï¿½rde siffror
 
     return(0);      //Avsluta och retunera 0
 }
@@ -25,21 +25,21 @@ float calc_resistance(int count, char conn, float *array)   //Funktionen
     float return_value=0, sum_denum=0;
     int x;
 
-    if(conn == 'P')         //Om connection är satt till parallel exekvera detta..
+    if(conn == 'P')         //Om connection ï¿½r satt till parallel exekvera detta..
     {
         for(x=0; x < count; x++)
         {
-            if(array[x] == 0)       //Om värdet är noll retunera 0
+            if(array[x] == 0)       //Om vï¿½rdet ï¿½r noll retunera 0
                 return(0.0);
             else
-                sum_denum += 1/array[x];        //Summera alla värden enligt formeln: 1/TOTres = 1/R1 + 1/R2 + 1/Rx ...
+                sum_denum += 1/array[x];        //Summera alla vï¿½rden enligt formeln: 1/TOTres = 1/R1 + 1/R2 + 1/Rx ...
         }
 
         return_value = 1/sum_denum;
 
         return(return_value);
     }
-    else if(conn == 'S')        //Om connection är satt till seriellt exekvera detta..
+    else if(conn == 'S')        //Om connection ï¿½r satt till seriellt exekvera detta..
     {
         for(x=0; x < count; x++)
         {
@@ -49,5 +49,5 @@ float calc_resistance(int count, char conn, float *array)   //Funktionen
         return(return_value);
     }
 
-    return(-1);             //Om metoden inte exekverar något av de två alternativen retunera -1 ( = Fel).
+    return(-1);             //Om metoden inte exekverar nï¿½got av de tvï¿½ alternativen retunera -1 ( = Fel).
 }
